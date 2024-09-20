c489655b7ad36644e99d2d080fe07d76b6d748bf
nazmul-2020 <nazmulcpi2012@gmail.com>
2024-09-20 17:24:36 +0600

    add Navber

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import Navbar from "@/components/Homepage/Navbar";
+// import Navbar from "@/components/Homepage/Navbar";
+import HomePage from "@/components/Homepage/HomePage";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,36 +10,9 @@ export default function Home() {
       <main
       // className="flex flex-col gap-8 row-start-2 items-center sm:items-start"
       >
-        <Navbar />
-        <h1>The next project start</h1>
-        <h1>The next project end</h1>
+        {/* <Navbar /> */}
+        <HomePage />
 
-        <div class="w-full relative">
-          <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <div class="w-full relative mx-auto h-auto overflow-hidden rounded-lg ">
-              <img src="https://pagedone.io/asset/uploads/1688031162.jpg" alt="image" class="w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110" />
-            </div>
-            <div class="w-full relative mx-auto h-auto overflow-hidden rounded-lg ">
-              <img src="https://pagedone.io/asset/uploads/1688031162.jpg" alt="image" class="w-full h-auto relative z-0 rounded-lg scale-110 transition-all duration-300 hover:scale-100" />
-            </div>
-          </div>
-        </div>
-
-
-        <div className="flex items-center justify-between gap-3 my-4">
-          <div class="w-32 h-32 rounded bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded</div>
-          <div class="w-32 h-32 rounded-t bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-t</div>
-          <div class="w-32 h-32 rounded-r bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-r</div>
-          <div class="w-32 h-32 rounded-b bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-b</div>
-          <div class="w-32 h-32 rounded-l bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-l</div>
-          <div class="w-32 h-32 rounded-tr bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-tr</div>
-          <div class="w-32 h-32 rounded-tl bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-tl</div>
-          <div class="w-32 h-32 rounded-br bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-br</div>
-          <div class="w-32 h-32 rounded-bl bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-bl</div>
-          <div class="w-32 h-32 rounded-none bg-indigo-500 flex items-center justify-center text-white text-base font-medium">rounded-none</div>
-
-
-        </div>
 
       </main>
     </div>
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+// import { IoIosArrowDown } from "react-icons/io";
+import { MdKeyboardArrowDown } from "@react-icons/all-files/md/MdKeyboardArrowDown";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class=" border-b border-black  w-full  bg-[#363636] text-white flex items-center  h-20">
+        <div class="container mx-auto">
+          <div class="w-full flex  flex-col lg:flex-row">
+            <div
+              class="hidden w-full lg:flex  lg:px-11 "
+              id="navbar-default-with-search"
+            >
+              <ul class="flex items-center flex-col mt-4 lg:mt-0 lg:flex-row gap-8">
+                <li className="flex items-center justify-cen ">
+                  <a
+                    href="javascript:;"
+                    class=" text-sm lg:text-base font-medium  mb- lg:mr-2 md:mb-0 md:mr-3"
+                  >
+                    Home
+                  </a>
+                  <MdKeyboardArrowDown className="text-lg mt-1.5" />
+                </li>
+                <li className="flex items-center justify-cen ">
+                  <a
+                    href="javascript:;"
+                    class=" text-sm lg:text-base font-medium  mb- lg:mr-2 md:mb-0 md:mr-3"
+                  >
+                    Event
+                  </a>
+                  <MdKeyboardArrowDown className="text-lg mt-1.5" />
+                </li>
+                <li className="flex items-center justify-cen ">
+                  <a
+                    href="javascript:;"
+                    class=" text-sm lg:text-base font-medium  mb- lg:mr-2 md:mb-0 md:mr-3"
+                  >
+                    Contact
+                  </a>
+                  <MdKeyboardArrowDown className="text-lg mt-1.5" />
+                </li>
+                <li className="flex items-center justify-cen ">
+                  <a
+                    href="javascript:;"
+                    class=" text-sm lg:text-base font-medium  mb- lg:mr-2 md:mb-0 md:mr-3"
+                  >
+                    Product
+                  </a>
+                  <MdKeyboardArrowDown className="text-lg mt-1.5" />
+                </li>
+              </ul>
+              <div class="flex items-center ml-auto justify-center">
+                <div class="relative ">
+                  <input
+                    type="text"
+                    id="input-group-search"
+                    class="block w-full py-2 px-4 pl-10 text-sm text-gray-900 placeholder-gray-500 border border-gray-300 rounded-full focus:outline-none "
+                    placeholder="Search "
+                  />
+                  <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="font-bold"
+                    >
+                      <path
+                        d="M15.75 15.75L14.25 14.25M15 8.25C15 11.9779 11.9779 15 8.25 15C4.52208 15 1.5 11.9779 1.5 8.25C1.5 4.52208 4.52208 1.5 8.25 1.5C11.9779 1.5 15 4.52208 15 8.25Z"
+                        stroke="#6B7280"
+                        stroke-width="1.2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
